955c8c2ebbe6640af93cb4d5f11b21d5905051f9
blkclct <paradox.h129@gmail.com>
2020-03-15 17:32:44 +0900

    :sparkles: Add: 電卓計算する関数を作成した

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,37 @@
-
 export function format(first: string, middle: string, last: string): string {
   return (
-    (first || '') +
-    (middle ? ` ${middle}` : '') +
-    (last ? ` ${last}` : '')
+    (first || "") + (middle ? ` ${middle}` : "") + (last ? ` ${last}` : "")
   );
 }
+
+export const calc = (input: string): number => {
+  const inputNumber = input.split(/\+|\-|\*|\//).map(s => parseInt(s));
+  const inputOperator = input.split(/\d+/).filter(s => s !== "");
+
+  if (inputNumber === null || inputOperator === null) {
+    return 0;
+  }
+  if (inputNumber.length <= 1 || inputOperator.length <= 0) {
+    return 0;
+  }
+  if (inputNumber.length - inputOperator.length !== 1) {
+    return 0;
+  }
+
+  return inputOperator.reduce((acc, cur, index) => {
+    return _calc(acc, inputNumber[index + 1], cur);
+  }, inputNumber[0]);
+};
+
+const _calc = (num1: number, num2: number, operator: string): number => {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+  }
+};
